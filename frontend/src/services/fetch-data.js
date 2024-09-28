4da4e144a7606b05ce072eeed27bde6c83becacf
Vaibhav D <vaibhavdudhal11@gmail.com>
2024-09-28 23:56:44 +0530

    init

--- frontend/src/services/fetch-data.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    try {
-        const response = await fetch('https://s3.amazonaws.com/roxiler.com/product_transaction.json');
-        const data = await response.json();
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
-}
