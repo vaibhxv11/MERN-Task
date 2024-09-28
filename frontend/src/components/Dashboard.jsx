592668e313c584a2592f28628c2db65ae7dd7a7f
Vaibhav D <vaibhavdudhal11@gmail.com>
2024-09-29 00:00:52 +0530

    init

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await fetch('https://cors-anywhere.herokuapp.com/https://s3.amazonaws.com/roxiler.com/product_transaction.json');
+        const response = await fetch('https://s3.amazonaws.com/roxiler.com/product_transaction.json', { mode: 'no-cors' });
 
         const data = await response.json();
         
